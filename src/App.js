c974d9259a238f4c4c4eb16551e369ced0fcbd93
Meelenych <meelenych@gmail.com>
2024-03-07 16:54:10 -0700

    year valiidation app using bootstrap styles

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import YearValidation from './YearValidation';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<YearValidation />
+		</div>
+	);
+}
+
+export default App;
